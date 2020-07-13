5efe7d14d21402d62a08e532206d494c83974e94
marioquiroga <mquiroga@dosalcubo.com>
2020-07-13 02:14:04 -0300

    Formulario component

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,40 @@ import React from 'react';
 
 const Formulario = () => {
     return (
-        <h1>Formulario</h1>
+        <form className='col-12'>
+            <fieldset className='text-center'>
+                <legend>Busca bebidas</legend>
+            </fieldset>
+
+            <div className='row'>
+                <div className='col-md-4'>
+                    <input
+                        name='nombre'
+                        className='form-control'
+                        type='text'
+                        placeholder='buscar por ingrediente'
+                    ></input>
+                </div>
+
+                <div className='col-md-4'>
+                    <select
+                        className='form-control'
+                        name='categoria'
+                    >
+                        <option value=''>--Selecciona--</option>
+                    </select>
+                </div>
+
+                <div className='col-md-4'>
+                    <input
+                        className='btn btn-primary btn-block'
+                        type='submit'
+                        value='Buscar'
+                    ></input>
+                </div>
+            </div>
+
+        </form>
     );
 }
  
