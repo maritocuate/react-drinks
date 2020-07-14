4c2bd5d1d4ffc1d2c87d36e416180886896eb081
marioquiroga <mquiroga@dosalcubo.com>
2020-07-14 01:33:58 -0300

    ModalContext call api by id

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,10 +1,26 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios'
 
 export const ModalContext = createContext()
 
 const ModalProvider = (props) => {
 
     const [id, setId] = useState(null)
+    const [recipe, setRecipe] = useState(null)
+
+    useEffect(() => {
+        
+        if(!id) return
+
+        const callApi = async () => {
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+            const result = await axios(url)
+
+            setRecipe(result.data.drinks[0])
+        }
+        callApi()
+
+    }, [id])
 
     return (
         <ModalContext.Provider value={{setId}}>
